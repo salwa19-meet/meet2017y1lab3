80e4c7df6730c6760ca2e2b2aa60d62eb7e91a52
salwa19-meet <salwa19@meet.mit.edu>
2017-07-25 18:14:48 +0300

    Stopping here

---
+++ meet2017module3.py
@@ -0,0 +1,25 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /home/student/salwa19_lab3/meet2017y1lab3/helloworld.py ======
+hello
+what is your namewilliam
+>>> a = "MEET"
+>>> b = "meet"
+>>> c = "Meat"
+>>> a == b
+False
+>>> a == c
+False
+>>> b == c
+False
+>>> a != b
+True
+>>> a =!c
+SyntaxError: invalid syntax
+>>> a != c
+True
+>>> b != c
+True
+>>> 
